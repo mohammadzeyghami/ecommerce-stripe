b86338208a8761d0a935469ffe4c4393df13ad81
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-22 00:51:09 +0100

    start filing

---
+++ components/Layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div className="hero-banner-container">
+      <p className="beats-solo">Text</p>
+      {/* test */}
+    </div>
+  );
+};
+
+export default Layout;
