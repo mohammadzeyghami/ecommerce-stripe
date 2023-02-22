21ed9d6dfc03abc25727fcb4d1e5fe74df2cd7bc
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-22 23:39:58 +0100

    update products

--- pages/index.js
+++ pages/index.js
@@ -25,10 +25,10 @@ export default function Home({ products, bannerData }) {
 }
 
 export const getServerSideProps = async () => {
-  const query = '*[_type == "product]"';
+  const query = '*[_type == "product"]';
   const products = await client.fetch(query);
 
-  const bannerQuery = '*[_type == "banner]"';
+  const bannerQuery = '*[_type == "banner"]';
   const bannerData = await client.fetch(bannerQuery);
 
   return {
