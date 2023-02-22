e76fb47d76fb34b7d8a52f6ebc4955545a7783f7
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-22 23:52:25 +0100

    some buttons

--- components/Herobanner.jsx
+++ components/Herobanner.jsx
@@ -5,13 +5,15 @@ const Herobanner = () => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">Small Text</p>
-        <h3>mid text</h3>
-        {/* <img src="" alt="headphones" className="hero-banner-image" /> */}
+        <div className="products-heading">
+          <h2>Best Seller Products</h2>
+          <p>speaker There are many variations passages</p>
+        </div>
+        <img src="" alt="headphones" className="hero-banner-image" />
         <div>
-          {/* <Link href="/product/ID">
+          <Link href="/product/ID">
             <button type="button">Button Text</button>
-          </Link> */}
+          </Link>
           <div className="desc">
             <h5>Description</h5>
             <p>Description </p>
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import Link from "next/link";
 const Layout = () => {
   return (
     <div className="hero-banner-container">
-      <p className="beats-solo">Text</p>
+      <p className="beats-solo">Small Text</p>
+      <h3>Mid Text</h3>
+      <Link href="/product/ID">
+        <button type="button">Button Text</button>
+      </Link>
       {/* test */}
     </div>
   );
