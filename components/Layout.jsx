86bb9aa9528ef9e031ac982895945b9bb75472fc
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-24 21:45:47 +0100

    herobanner

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,16 +4,27 @@ import Image from "next/image";
 import { urlFor } from "../lib/client";
 const Layout = ({ heroBanner }) => {
   return (
-    <div className="hero-banner-container">
-      <p className="beats-solo">{heroBanner.smallText}</p>
-      <p className="beats-solo">Small Text</p>
-      <h3>{heroBanner.midText}</h3>
-      <h1>{heroBanner.largeText1}</h1>
-      <Image src={urlFor(heroBanner.image)} alt="imageee" />
-      <Link href={`/product/${heroBanner.product}`}>
-        <button type="button">{heroBanner.buttonText}</button>
-      </Link>
-      {/* test sssssssssssssssssssss*/}
+    <div className="hero-banner-container ">
+      <div className="hero-banner-wrapper">
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="headphones"
+          className="hero-banner-image"
+        />
+
+        <div>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
