af42d8cb513cfad1fe146dddf24ae677321a7d50
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-26 09:57:04 +0100

    add to cart logic

---
+++ context/StateContext.js
@@ -0,0 +1,60 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
+
+const Context = createContext();
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState();
+  const [totalQuantities, setTotalQuantites] = useState(0);
+  const [qty, setQty] = useState(1);
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    );
+    setTotalPrice(
+      (pervTotalPrice) => pervTotalPrice + product.price * quantity
+    );
+    setTotalQuantites(
+      (pervTotalQuantities) => pervTotalQuantities + product.quantity
+    );
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return { ...cartProduct, quantity: cartProduct.quantity + quantity };
+      });
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+      setCartItems([...cartItems, { ...product }]);
+    }
+    toast.success(`${qty} ${product.name} added to the cart.`);
+  };
+  const incQty = () => {
+    setQty((pervQty) => pervQty + 1);
+  };
+  const decQty = () => {
+    setQty((pervQty) => {
+      if (pervQty - 1 < 1) return 1;
+      return pervQty - 1;
+    });
+  };
+  return (
+    <Context.Provider
+      value={{
+        showCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        incQty,
+        decQty,
+        onAdd,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(Context);
