e9d42eae5b6f086c4a0fa3ca45a874c2b084f9e4
mohammadzeyghami <mohammadzeighami197@gmail.com>
2023-02-27 18:48:40 +0100

    stripe

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -26,6 +26,7 @@ const ProductDetails = ({ product, products }) => {
           <div>
             {image?.map((item, i) => (
               <img
+                key={i}
                 src={urlFor(item)}
                 className={
                   i === index ? "small-image selected-image" : "small-image"
